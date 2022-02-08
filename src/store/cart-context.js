86d2c245d7e29f55cd9f9c624a13dc30a7785f95
popietree <96894197+popietree@users.noreply.github.com>
2022-02-07 16:49:35 -0800

    Add files via upload

---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+  clearCart: (items) => {},
+});
+
+export default CartContext;
